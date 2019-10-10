9c4e8206e3f2b3c038806bc9a6d2aa405168d308
kate <kirilek@bk.ru>
2019-10-10 20:49:44 +0300

    fixed test with empty values with wrapper.find

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -9,15 +9,21 @@ import {fakeOrders} from '../mock/fakeOrders';
 configure({ adapter: new Adapter() });
 
 describe('Order component', () => {
-  it('render with empty state', () => {
-    const wrapper = shallow(<Order  key={""} order={""}/>);
-    // wrapper.setProps({key: "", order: ""});
-    expect(toJson(wrapper)).toMatchSnapshot();
-  });
+//   it('render with empty state', () => {
+//     const wrapper = shallow(<Order order={""}/>);
+//     // wrapper.setProps({key: "", order: ""});
+//     expect(toJson(wrapper)).toMatchSnapshot();
+//   });
 
   it('render with fakecomponent', () => {
     const wrapper = shallow(<Order key={0} order={fakeOrders[0]}/>);
     // wrapper.setProps({key: 0, order: fakeOrders[0]});
     expect(toJson(wrapper)).toMatchSnapshot();
    });
+
+   it('check empty values', () => {
+    const wrapper = shallow(<Order order={fakeOrders[0]} />);
+    wrapper.setProps({order: {}});
+    expect(wrapper.find(".Order-items").children().length).toEqual(0);
+  });
 });
